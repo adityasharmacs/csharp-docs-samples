a46f22067d03b073035dd12aa465da8317ab7c2b
Jeffrey Rennie <rennie@google.com>
2016-05-19 15:24:56 -0700

    Run Format-Code.

--- bigquery/api/Export/Export/Program.cs
+++ bigquery/api/Export/Export/Program.cs
@@ -1,4 +1,18 @@
-﻿using CommandLine;
+﻿// Copyright (c) 2016 Google Inc.
+// 
+// Licensed under the Apache License, Version 2.0 (the "License"); you may not
+// use this file except in compliance with the License. You may obtain a copy of
+// the License at
+// 
+// http://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+// WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+// License for the specific language governing permissions and limitations under
+// the License.
+
+using CommandLine;
 using Google.Apis.Auth.OAuth2;
 using Google.Apis.Bigquery.v2;
 using Google.Apis.Services;
@@ -31,7 +45,7 @@ namespace GoogleCloudSamples
             HelpText = "The output format.  One of CSV, NEWLINE_DELIMITED_JSON, or AVRO.")]
         public string ExportFormat { get; set; }
 
-        [Option('c', "Compression", DefaultValue = "NONE", 
+        [Option('c', "Compression", DefaultValue = "NONE",
             HelpText = "Format to compress result with.  One of NONE or GZIP")]
         public string Compression { get; set; }
 
@@ -41,7 +55,6 @@ namespace GoogleCloudSamples
             return HelpText.AutoBuild(this,
               (HelpText current) => HelpText.DefaultParsingErrorsHandler(this, current));
         }
-
     }
 
     public class Program
