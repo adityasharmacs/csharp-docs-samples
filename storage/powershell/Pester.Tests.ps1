6c871b3ad312a337f922722a455bb43a9f9647ab
Jeffrey Rennie <rennie@google.com>
2016-07-11 14:43:01 -0700

    Pester tests are a hassle.
    I get this error message:
     [-] uploads a new directory. 6.82s
       Expected: {testdata/ testdata/hello.txt testdata/a/ testdata/a/b/ testdata/a/b/c.txt testdata/a/empty/}
       But was:  {testdata/ testdata/hello.txt testdata/a/ testdata/a/b/ testdata/a/b/c.txt testdata/a/empty/}
       42:         , $objectNames | Should Be (Groom-Output "testdata/
       at <ScriptBlock>, C:\Users\Jeffrey Rennie\gitrepos\dotnet-docs-samples\storage\powershell\Pester.Tests.ps1: line 42

---
+++ storage/powershell/Pester.Tests.ps1
@@ -0,0 +1,49 @@
+﻿$here = Split-Path -Parent $MyInvocation.MyCommand.Path
+$sut = (Split-Path -Leaf $MyInvocation.MyCommand.Path) -replace '\.Tests\.', '.'
+. "$here\$sut"
+
+function Clear-GcsTestDir {
+    $objects = Find-GcsObject -Bucket $env:GOOGLE_BUCKET -Prefix testdata/
+    Write-Progress -Activity "Removing old objects" `
+        -CurrentOperation "Finding objects" -PercentComplete 0
+    foreach ($object in $objects) {
+        Write-Progress -Activity "Removing old objects" `
+            -CurrentOperation "Removing $($object.Name)" `
+            -PercentComplete (++$progress * 100 / $objects.Length) `
+            -Completed:($progress -eq $objects.Length)
+        $ignore = Remove-GcsObject -Bucket $env:GOOGLE_BUCKET `
+            -ObjectName $object.Name
+    }
+}
+
+function Upload-Testdata([switch]$PassThru) {
+    $output = .\Copy-GcsObject.ps1 testdata gs://$env:GOOGLE_BUCKET/testdata -Recurse
+    if ($PassThru) {
+        $output
+    }
+}
+
+function Groom-Output($output) {
+    $output.Split("`n") | ForEach-Object { $_.Trim() }
+}
+
+function Groom-Input {
+    ($input | ForEach-Object { $_ }) -join "`n"
+}
+
+Describe "pester" {
+    It "does something useful" {
+        $true | Should Be $false
+    }
+
+    It "uploads a new directory." {
+        Clear-GcsTestDir
+        $objectNames = (Upload-Testdata -PassThru).Name
+        , $objectNames | Should Be (Groom-Output "testdata/
+        testdata/hello.txt
+        testdata/a/
+        testdata/a/b/
+        testdata/a/b/c.txt
+        testdata/a/empty/")
+    }
+}
