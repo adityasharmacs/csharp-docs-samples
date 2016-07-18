f226e417e4b133b5e300cd455df61ce30bb82128
Jeffrey Rennie <rennie@google.com>
2016-07-18 15:42:22 -0700

    Drop the timeout to 2 minutes.

--- BuildTools.psm1
+++ BuildTools.psm1
@@ -333,7 +333,7 @@ function Run-TestScripts
                 throw "FAILED with exit code $LASTEXITCODE"
             }
         }
-        if (Wait-Job $job -Timeout 300) {
+        if (Wait-Job $job -Timeout 120) {
             Receive-Job $job
             if ($job.State -eq 'Failed') {
                 $failures += $relativePath
