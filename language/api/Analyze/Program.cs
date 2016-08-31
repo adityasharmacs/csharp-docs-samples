efcdbc428259d02c54e2aaf332f6567a94a9930e
Jeffrey Rennie <rennie@google.com>
2016-08-31 10:19:24 -0700

    AnalyzeEverything

--- language/api/Analyze/Program.cs
+++ language/api/Analyze/Program.cs
@@ -16,6 +16,7 @@ using Google.Apis.CloudNaturalLanguageAPI.v1beta1;
 using Google.Apis.Auth.OAuth2;
 using Google.Apis.Services;
 using Google.Apis.CloudNaturalLanguageAPI.v1beta1.Data;
+using System.Collections.Generic;
 
 namespace Analyze
 {
@@ -41,7 +42,7 @@ namespace Analyze
             return new CloudNaturalLanguageAPIService(serviceInitializer);
         }
 
-        static void AnalyzeEntities(string text, string encoding="UTF16")
+        static void AnalyzeEntities(string text, string encoding = "UTF16")
         {
             var service = CreateNaturalLanguageAPIClient();
             var response = service.Documents.AnalyzeEntities(
@@ -54,20 +55,23 @@ namespace Analyze
                     },
                     EncodingType = encoding
                 }).Execute();
-            string entity_separator = "";
-            foreach (var entity in response.Entities)
+            WriteEntities(response.Entities);
+        }
+
+        static void WriteEntities(IEnumerable<Entity> entities)
+        {
+            Console.WriteLine("Entities:");
+            foreach (var entity in entities)
             {
-                Console.WriteLine($"Name: {entity.Name}");
-                Console.WriteLine($"Type: {entity.Type}");
-                Console.WriteLine($"Salience: {entity.Salience}");
-                Console.WriteLine("Mentions:");
+                Console.WriteLine($"\tName: {entity.Name}");
+                Console.WriteLine($"\tType: {entity.Type}");
+                Console.WriteLine($"\tSalience: {entity.Salience}");
+                Console.WriteLine("\tMentions:");
                 foreach(var mention in entity.Mentions)
-                    Console.WriteLine($"\t{mention.Text.BeginOffset}: {mention.Text.Content}");
-                Console.WriteLine("Metadata:");
+                    Console.WriteLine($"\t\t{mention.Text.BeginOffset}: {mention.Text.Content}");
+                Console.WriteLine("\tMetadata:");
                 foreach (var keyval in entity.Metadata)
-                    Console.WriteLine($"\t{keyval.Key}: {keyval.Value}");
-                Console.Write(entity_separator);
-                entity_separator = "\n";
+                    Console.WriteLine($"\t\t{keyval.Key}: {keyval.Value}");
             }
         }
 
@@ -83,8 +87,13 @@ namespace Analyze
                         Type = "PLAIN_TEXT"
                     },
                 }).Execute();
-            Console.WriteLine($"Polarity: {response.DocumentSentiment.Polarity}");
-            Console.WriteLine($"Magnitude: {response.DocumentSentiment.Magnitude}");
+            WriteSentiment(response.DocumentSentiment);
+        }
+
+        static void WriteSentiment(Sentiment sentiment)
+        {
+            Console.WriteLine($"Polarity: {sentiment.Polarity}");
+            Console.WriteLine($"Magnitude: {sentiment.Magnitude}");
         }
 
         static void AnalyzeSyntax(string text, string encoding = "UTF16")
@@ -104,16 +113,46 @@ namespace Analyze
                         ExtractSyntax = true
                     }
                 }).Execute();
+            WriteSentences(response.Sentences);
+        }
+
+        static void WriteSentences(IEnumerable<Sentence> sentences)
+        {
             Console.WriteLine("Sentences:");
-            foreach (var sentence in response.Sentences)
+            foreach (var sentence in sentences)
             {
                 Console.WriteLine($"\t{sentence.Text.BeginOffset}: {sentence.Text.Content}");
             }
         }
 
+        static void AnalyzeEverything(string text, string encoding = "UTF16")
+        {
+            var service = CreateNaturalLanguageAPIClient();
+            var response = service.Documents.AnnotateText(
+                new AnnotateTextRequest()
+                {
+                    Document = new Document()
+                    {
+                        Content = text,
+                        Type = "PLAIN_TEXT"
+                    },
+                    EncodingType = encoding,
+                    Features = new Features()
+                    {
+                        ExtractSyntax = true,
+                        ExtractDocumentSentiment = true,
+                        ExtractEntities = true,
+                    }
+                }).Execute();
+            Console.WriteLine($"Language: {response.Language}");
+            WriteSentiment(response.DocumentSentiment);
+            WriteSentences(response.Sentences);
+            WriteEntities(response.Entities);
+        }
+
         static void Main(string[] args)
         {
-            AnalyzeSyntax("The rain in Spain stays mainly in the plain.");
+            AnalyzeEverything("The rain in Spain stays mainly in the plain.");
         }
     }
 }
