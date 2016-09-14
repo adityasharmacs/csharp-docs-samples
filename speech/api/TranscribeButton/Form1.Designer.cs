55b156bce76f5dbc126643245235ddd332838182
Jeffrey Rennie <rennie@google.com>
2016-09-14 16:39:26 -0700

    Speech button Windows Forms app.

---
+++ speech/api/TranscribeButton/Form1.Designer.cs
@@ -0,0 +1,62 @@
+﻿namespace TranscribeButton
+{
+    partial class TranscribeForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Transcription = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // Transcription
+            // 
+            this.Transcription.Enabled = false;
+            this.Transcription.Location = new System.Drawing.Point(12, 138);
+            this.Transcription.Multiline = true;
+            this.Transcription.Name = "Transcription";
+            this.Transcription.Size = new System.Drawing.Size(265, 243);
+            this.Transcription.TabIndex = 1;
+            // 
+            // TranscribeForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(291, 397);
+            this.Controls.Add(this.Transcription);
+            this.Name = "TranscribeForm";
+            this.Text = "Transcribe Demo";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.OnKeyDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.OnKeyUp);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox Transcription;
+    }
+}
+
---
+++ speech/api/TranscribeButton/Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TranscribeButton
+{
+    public partial class TranscribeForm : Form
+    {
+        public TranscribeForm()
+        {
+            InitializeComponent();
+        }
+
+        private void OnKeyDown(object sender, KeyEventArgs e)
+        {
+            Transcription.Text = $"Key Down: {e.KeyValue}";   
+        }
+
+        private void OnKeyUp(object sender, KeyEventArgs e)
+        {
+            Transcription.Text = $"Key Up: {e.KeyValue}";
+        }
+
+    }
+}
