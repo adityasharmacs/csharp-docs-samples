33a7589baa501ce9f2aacf7aa1c9a49cfd47cdf8
Jeffrey Rennie <rennie@google.com>
2016-09-15 15:51:15 -0700

    Trying to build a responsive UI with NAudio is a challenge.

--- speech/api/TranscribeButton/Form1.Designer.cs
+++ speech/api/TranscribeButton/Form1.Designer.cs
@@ -29,6 +29,7 @@
         private void InitializeComponent()
         {
             this.Transcription = new System.Windows.Forms.TextBox();
+            this.DeviceList = new System.Windows.Forms.ListBox();
             this.SuspendLayout();
             // 
             // Transcription
@@ -40,14 +41,26 @@
             this.Transcription.Size = new System.Drawing.Size(265, 243);
             this.Transcription.TabIndex = 1;
             // 
+            // DeviceList
+            // 
+            this.DeviceList.FormattingEnabled = true;
+            this.DeviceList.Location = new System.Drawing.Point(12, 17);
+            this.DeviceList.Name = "DeviceList";
+            this.DeviceList.Size = new System.Drawing.Size(264, 95);
+            this.DeviceList.TabIndex = 2;
+            this.DeviceList.KeyDown += new System.Windows.Forms.KeyEventHandler(this.OnKeyDown);
+            this.DeviceList.KeyUp += new System.Windows.Forms.KeyEventHandler(this.OnKeyUp);
+            // 
             // TranscribeForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(291, 397);
+            this.Controls.Add(this.DeviceList);
             this.Controls.Add(this.Transcription);
             this.Name = "TranscribeForm";
             this.Text = "Transcribe Demo";
+            this.Load += new System.EventHandler(this.OnLoad);
             this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.OnKeyDown);
             this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.OnKeyUp);
             this.ResumeLayout(false);
@@ -57,6 +70,7 @@
 
         #endregion
         private System.Windows.Forms.TextBox Transcription;
+        private System.Windows.Forms.ListBox DeviceList;
     }
 }
 
--- speech/api/TranscribeButton/Form1.cs
+++ speech/api/TranscribeButton/Form1.cs
@@ -7,11 +7,24 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using NAudio.Wave;
 
 namespace TranscribeButton
 {
+    enum WhatsHappening
+    {
+        Initializing,
+        Nothing,
+        Recording,
+        WaitingForFinalData,
+        Transcribing,
+    };
     public partial class TranscribeForm : Form
     {
+        WhatsHappening _whatsHappening = WhatsHappening.Nothing;       
+        private WaveIn _waveIn;
+        private List<byte> _buffer = new List<byte>();
+
         public TranscribeForm()
         {
             InitializeComponent();
@@ -19,13 +32,66 @@ namespace TranscribeButton
 
         private void OnKeyDown(object sender, KeyEventArgs e)
         {
-            Transcription.Text = $"Key Down: {e.KeyValue}";   
+            if (e.KeyValue == 32 && _whatsHappening == WhatsHappening.Nothing)
+            {
+                _whatsHappening = WhatsHappening.Recording;
+                _buffer.Clear();
+                Transcription.Text = "Recording...";
+            }
+            else
+            {
+                Transcription.Text = $"Key Down: {e.KeyValue}";
+            }
+        }
+
+        private void WaveIn_OnDataAvailable(object sender, WaveInEventArgs e)
+        {
+            if (_whatsHappening == WhatsHappening.Recording)
+                _buffer.AddRange(e.Buffer.Take(e.BytesRecorded));
+        }
+
+        private void WaveIn_OnRecordingStopped(object sender, EventArgs e)
+        {
         }
 
         private void OnKeyUp(object sender, KeyEventArgs e)
         {
-            Transcription.Text = $"Key Up: {e.KeyValue}";
+            if (e.KeyValue == 32 && _whatsHappening == WhatsHappening.Recording)
+            {
+                _whatsHappening = WhatsHappening.Transcribing;
+                Transcription.Text = "Transcribing...";
+            }
+            else
+            {
+                Transcription.Text = $"Key Up: {e.KeyValue}";
+            }
         }
 
+        private void InvokeAction(Action a)
+        {
+            Delegate d = a;
+            Invoke(d);
+        }
+
+        private void OnLoad(object sender, EventArgs e)
+        {
+            for (int i = 0; i < WaveIn.DeviceCount; ++i)
+            {
+                DeviceList.Items.Add(WaveIn.GetCapabilities(i).ProductName);
+                DeviceList.SelectedIndex = 0;
+            }
+                InvokeAction(() => { Transcription.Text = "Initializing Audio..."; });
+                _whatsHappening = WhatsHappening.Initializing;
+                _waveIn = new WaveIn()
+                {                    
+                    DeviceNumber = DeviceList.SelectedIndex,
+                    WaveFormat = new WaveFormat(sampleRate: 8000, channels: 1),
+                };
+                _waveIn.DataAvailable += WaveIn_OnDataAvailable;
+                _buffer.Clear();
+                _whatsHappening = WhatsHappening.Nothing;
+                _waveIn.StartRecording();
+                InvokeAction(() => { Transcription.Text = "Ready.  Hold down the spacebar to record."; });           
+        }
     }
 }
