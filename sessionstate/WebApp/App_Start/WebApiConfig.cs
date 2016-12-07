516a044fadf52d431217abf1e67b8c741df628ac
Jeffrey Rennie <rennie@google.com>
2016-12-07 14:33:22 -0800

    Add Session to WebApi.

--- sessionstate/WebApp/App_Start/WebApiConfig.cs
+++ sessionstate/WebApp/App_Start/WebApiConfig.cs
@@ -7,16 +7,14 @@ namespace WebApp
 {
     public static class WebApiConfig
     {
+        public static string UrlPrefix = "api";
+        public static string UrlPrefixRelative = "~/api";
+
         public static void Register(HttpConfiguration config)
         {
-            // Web API configuration and services
-
-            // Web API routes
-            config.MapHttpAttributeRoutes();
-
             config.Routes.MapHttpRoute(
                 name: "DefaultApi",
-                routeTemplate: "api/{controller}/{id}",
+                routeTemplate: WebApiConfig.UrlPrefix + "/{controller}/{id}",
                 defaults: new { id = RouteParameter.Optional }
             );
         }
