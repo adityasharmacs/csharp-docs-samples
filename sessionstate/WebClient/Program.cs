49f18594286e2e691c361b6770e902681462682f
Jeffrey Rennie <rennie@google.com>
2017-01-03 15:58:09 -0800

    Lint

--- sessionstate/WebClient/Program.cs
+++ sessionstate/WebClient/Program.cs
@@ -121,7 +121,7 @@ namespace WebClient
             stopwatch.Start();
             for (int i = 0; i < tasks.Length; ++i)
             {
-                tasks[i] = Task.Run(async () => await TaskMainAsync(baseAddress, 
+                tasks[i] = Task.Run(async () => await TaskMainAsync(baseAddress,
                     options.Delay, options.SizeMultiplier));
             }
             Task.WaitAll(tasks);
