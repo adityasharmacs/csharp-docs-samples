c63158faa53235a99c8c093c70b2e7308f0ca8ee
Jeffrey Rennie <rennie@google.com>
2017-01-12 16:52:00 -0800

    Incomplete Recognize sample.

---
+++ speech/api/Recognize/Recognize.cs
@@ -0,0 +1,52 @@
+﻿using CommandLine;
+using Google.Cloud.Speech.V1Beta1;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GoogleCloudSamples
+{
+    class Options
+    {
+        [Option('a', "async", Default = false, HelpText =
+            "Make an asynchronous request", Required = false)]
+        public bool Async { get; set; }
+
+        [Option('m', "stream", Default = false, HelpText =
+            "Stream the request a few kilobytes at a time. Simulates audio"
+            + "arriving from a microphone.", Required = false)]
+        public bool Stream { get; set; }
+
+        [Value(0, HelpText = "A path to a sound file.", Required = true)]
+        public string FilePath { get; set; }
+
+    }
+    class Recognize
+    {
+        static void SyncRecognize(string filePath)
+        {
+            var speech = SpeechClient.Create();
+            var response = speech.SyncRecognize(new RecognitionConfig()
+            {
+                Encoding = RecognitionConfig.Types.AudioEncoding.Linear16,
+                SampleRate = 16000,
+            }, RecognitionAudio.FromFile(filePath));
+            foreach (var result in response.Results)
+            {
+                foreach (var alternative in result.Alternatives)
+                {
+                    Console.WriteLine(alternative.Transcript);
+                }
+            }
+        }
+        
+        static void Main(string[] args)
+        {
+            var result = Parser.Default.ParseArguments<Options>(args);
+            var options = result.WithParsed<Options>()
+            SyncRecognize(args[0]);
+        }
+    }
+}
