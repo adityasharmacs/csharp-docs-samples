af7d89e9e274fb31774bbe53ea5ee258c539d52b
Jeffrey Rennie <rennie@google.com>
2017-01-18 15:41:29 -0800

    speech_streaming_recognize doesn't work.

--- speech/api/Recognize/Recognize.cs
+++ speech/api/Recognize/Recognize.cs
@@ -2,6 +2,9 @@
 using Google.Cloud.Speech.V1Beta1;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.IO.Pipes;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -22,6 +25,9 @@ namespace GoogleCloudSamples
         + "file, and waits for the job to complete.")]
     class AsyncOptions : Options { }
 
+    [Verb("stream", HelpText = "Detects speech in an audio file by streaming "
+        + "it to the Speech API.")]
+    class StreamOptions : Options { }
 
     class Recognize
     {
@@ -67,12 +73,45 @@ namespace GoogleCloudSamples
         }
         // [END speech_async_recognize]
 
+        // [START speech_streaming_recognize]
+        static object StreamingRecognize(string filePath)
+        {
+            var speech = SpeechClient.Create();
+            var writeStream = new AnonymousPipeServerStream();
+            var readStream = new AnonymousPipeClientStream(
+                writeStream.GetClientHandleAsString());
+            var audio = RecognitionAudio.FromStream(readStream);
+            var longOperation = speech.AsyncRecognize(new RecognitionConfig()
+            {
+                Encoding = RecognitionConfig.Types.AudioEncoding.Linear16,
+                SampleRate = 16000,
+            }, audio);
+            longOperation = longOperation.PollOnce();
+            Debug.Assert(!longOperation.IsCompleted);
+            using (var f = new FileStream(filePath, FileMode.Open))
+            {
+                f.CopyTo(writeStream);
+                writeStream.Close();
+            }
+            var response = longOperation.Result;
+            foreach (var result in response.Results)
+            {
+                foreach (var alternative in result.Alternatives)
+                {
+                    Console.WriteLine(alternative.Transcript);
+                }
+            }
+            return 0;
+        }
+        // [END speech_streaming_recognize]
+
         static void Main(string[] args)
         {
             Parser.Default.ParseArguments<
-                SyncOptions, AsyncOptions>(args).MapResult(
+                SyncOptions, AsyncOptions, StreamOptions>(args).MapResult(
                 (SyncOptions opts) => SyncRecognize(opts.FilePath),
                 (AsyncOptions opts) => AsyncRecognize(opts.FilePath),
+                (StreamOptions opts) => StreamingRecognize(opts.FilePath),
                 errs => 1);
         }
     }
