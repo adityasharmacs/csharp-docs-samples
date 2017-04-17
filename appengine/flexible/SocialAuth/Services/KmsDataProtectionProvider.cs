16e0346333d44ea2601ae6586b69072d7be23e1d
Jeffrey Rennie <rennie@google.com>
2017-04-17 16:26:01 -0700

    wip

---
+++ appengine/flexible/SocialAuth/Services/KmsDataProtectionProvider.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.DataProtection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SocialAuth.Services
+{
+    public class KmsDataProtectionProvider : IDataProtectionProvider
+    {
+        IDataProtector IDataProtectionProvider.CreateProtector(string purpose)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
