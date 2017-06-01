f9b3577221a3f9b46213d726c39fbeccb4b72109
Jeffrey Rennie <rennie@google.com>
2017-06-01 14:23:13 -0700

    Cloud foundry step 3: service account.

--- powershell/cloud-foundry.ps1
+++ powershell/cloud-foundry.ps1
@@ -1,8 +1,40 @@
-﻿# From http://docs.pivotal.io/pivotalcf/1-8/customizing/gcp-prepare-env.html
+﻿###############################################################################
+# $projectId = 'surferjeff-cloudfoundry'
+
+# From http://docs.pivotal.io/pivotalcf/1-8/customizing/gcp-prepare-env.html
 gcloud compute networks create opsmgr --mode=custom
-gcloud compute networks subnets create opsmgr-subnet --network=opsmgr --range=10.0.0.0/20
+gcloud compute networks subnets create `
+    opsmgr-subnet --network=opsmgr --range=10.0.0.0/20
 # New-GceFirewallProtocol tcp -Port 0..65535 | New-GceFirewallProtocol udp -Port 0..65535 | New-GceFirewallProtocol icmp | Add-GceFirewall all-internal -Network opsmgr -SourceRange 10.0.0.0/20
-gcloud compute firewall-rules create all-internal '--allow=tcp:0-65535,udp:0-65535,icmp' --network=opsmgr --source-ranges=10.0.0.0/20
-gcloud compute firewall-rules create pcf-opsmanager '--allow=tcp:22,tcp:80,tcp:443' --network=opsmgr --source-ranges=0.0.0.0/0 --target-tags=pcf-opsmanager
-gcloud compute firewall-rules create pcf-lb '--allow=tcp:80,tcp:443,tcp:2222,tcp:8080' --network=opsmgr --source-ranges=0.0.0.0/0 --target-tags=pcf-lb
-gcloud compute firewall-rules create pcf-tcp-lb '--allow=tcp:1024-65535' --network=opsmgr --source-ranges=0.0.0.0/0 --target-tags=pcf-tcp-router
+gcloud compute firewall-rules create all-internal `
+    '--allow=tcp:0-65535,udp:0-65535,icmp' --network=opsmgr `
+    --source-ranges=10.0.0.0/20
+gcloud compute firewall-rules create pcf-opsmanager `
+    '--allow=tcp:22,tcp:80,tcp:443' --network=opsmgr `
+    --source-ranges=0.0.0.0/0 --target-tags=pcf-opsmanager
+gcloud compute firewall-rules create pcf-lb `
+    '--allow=tcp:80,tcp:443,tcp:2222,tcp:8080' --network=opsmgr `
+    --source-ranges=0.0.0.0/0 --target-tags=pcf-lb
+gcloud compute firewall-rules create pcf-tcp-lb `
+    '--allow=tcp:1024-65535' --network=opsmgr `
+    --source-ranges=0.0.0.0/0 --target-tags=pcf-tcp-router
+
+gcloud iam service-accounts create boshsa
+gcloud projects add-iam-policy-binding $projectId `
+    --member=serviceAccount:boshsa@$projectId.iam.gserviceaccount.com `
+    --role=roles/iam.serviceAccountActor
+gcloud projects add-iam-policy-binding $projectId `
+    --member=serviceAccount:boshsa@$projectId.iam.gserviceaccount.com `
+    --role=roles/compute.instanceAdmin
+gcloud projects add-iam-policy-binding $projectId `
+    --member=serviceAccount:boshsa@$projectId.iam.gserviceaccount.com `
+    --role=roles/compute.instanceAdmin.v1
+gcloud projects add-iam-policy-binding $projectId `
+    --member=serviceAccount:boshsa@$projectId.iam.gserviceaccount.com `
+    --role=roles/compute.networkAdmin
+gcloud projects add-iam-policy-binding $projectId `
+    --member=serviceAccount:boshsa@$projectId.iam.gserviceaccount.com `
+    --role=roles/compute.storageAdmin
+gcloud projects add-iam-policy-binding $projectId `
+    --member=serviceAccount:boshsa@$projectId.iam.gserviceaccount.com `
+    --role=roles/storage.admin
