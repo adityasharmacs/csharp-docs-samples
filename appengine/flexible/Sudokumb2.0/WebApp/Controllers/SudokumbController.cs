f87369f7859f8d90641770bf471c9ea6db4272d2
Jeffrey Rennie <rennie@google.com>
2018-02-07 15:04:16 -0800

    WIP
    Change-Id: I1d2daece72f651845bf44e42d60b47edd736d512

--- appengine/flexible/Sudokumb2.0/WebApp/Controllers/SudokumbController.cs
+++ appengine/flexible/Sudokumb2.0/WebApp/Controllers/SudokumbController.cs
@@ -5,8 +5,13 @@ namespace WebApp.Controllers
 {
     public class SudokumbController : Controller
     {
-        [Authorize(Roles="admin")]
         public IActionResult Index()
+        {
+            return View();
+        }
+
+        [Authorize(Roles="admin")]
+        public IActionResult Admin()
         {
             return View();
         }
---
+++ appengine/flexible/Sudokumb2.0/WebApp/Models/SudokumbViewModels/LoginViewModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApp.Models.SudokumbViewModels
+{
+    public class PuzzleAttribute : ValidationAttribute
+    {
+        public override bool IsValid(object value)
+        {
+            
+        }
+
+    }
+
+    public class IndexViewModel
+    {
+        const string samplePuzzle = "1 2 3   . . .   7 8 9\n. . .   . . .   . . .\n. . .   . . .   . . .\n\n. . .   4 . .   . . .                \n. 7 .   . 5 .   . . .\n. . .   . . 6   . 2 .\n\n. . 1   . . .   . . .\n. 5 .   . . 3   . . .\n3 . .   . . .   1 . .\n";
+
+        [Required]
+        [Puzzle]
+        [DataType(DataType.MultilineText)]
+        [Display(Prompt = samplePuzzle)]
+
+        public string Puzzle { get; set; }
+    }
+}
