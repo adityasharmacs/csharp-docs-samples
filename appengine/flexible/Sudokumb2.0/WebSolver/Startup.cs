b6700190898d8616b36c14ed43df1cf754290e85
Jeffrey Rennie <rennie@google.com>
2018-03-19 16:15:07 -0700

    Instrument WebSolver with Stackdriver.
    Change-Id: I63bbda8e10f229c0a7c56f0bfc2ce92090d3e63f

--- appengine/flexible/Sudokumb2.0/WebSolver/Startup.cs
+++ appengine/flexible/Sudokumb2.0/WebSolver/Startup.cs
@@ -1,11 +1,11 @@
 // Copyright (c) 2018 Google LLC.
-// 
+//
 // Licensed under the Apache License, Version 2.0 (the "License"); you may not
 // use this file except in compliance with the License. You may obtain a copy of
 // the License at
-// 
+//
 // http://www.apache.org/licenses/LICENSE-2.0
-// 
+//
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
 // WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
@@ -17,12 +17,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Google.Cloud.Datastore.V1;
+using Google.Cloud.Diagnostics.AspNetCore;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
 using Sudokumb;
 
 namespace WebSolver
@@ -39,6 +41,21 @@ namespace WebSolver
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            // Add exception logging.
+            services.AddGoogleExceptionLogging(options =>
+            {
+                options.ProjectId = Configuration["Google:ProjectId"];
+                options.ServiceName = Configuration["Google:AppEngine:ServiceName"] ?? "WebSolver";
+                options.Version = Configuration["Google:AppEngine:Version"] ?? "0.0";
+            });
+            // Add trace service.
+            services.AddGoogleTrace(options =>
+            {
+                options.ProjectId = Configuration["Google:ProjectId"];
+                options.Options = Google.Cloud.Diagnostics.Common.TraceOptions.Create(
+                    bufferOptions: Google.Cloud.Diagnostics.Common.BufferOptions.NoBuffer());
+            });
+
             services.AddOptions();
             services.Configure<PubsubGameBoardQueueOptions>(
                 Configuration.GetSection("Google"));
@@ -55,8 +72,18 @@ namespace WebSolver
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env,
+            ILoggerFactory loggerFactory)
         {
+            // Configure logging service.
+            loggerFactory.AddGoogle(Configuration["Google:ProjectId"]);
+
+            // Configure error reporting service.
+            app.UseGoogleExceptionLogging();
+
+            // Configure trace service.
+            app.UseGoogleTrace();
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
