e08830b553b033313d66a3f5c8746b7a3131ecc9
Jeffrey Rennie <rennie@google.com>
2018-03-20 12:41:26 -0700

    More roles needed.
    Change-Id: I5193ab5543b2f7c84b2c412145abea5eb8e1dfbf

--- appengine/flexible/Sudokumb2.0/Add-KmsPermissionsToAppEngine.ps1
+++ appengine/flexible/Sudokumb2.0/Add-KmsPermissionsToAppEngine.ps1
@@ -1,11 +1,11 @@
 ﻿# Copyright (c) 2018 Google LLC.
-# 
+#
 # Licensed under the Apache License, Version 2.0 (the "License"); you may not
 # use this file except in compliance with the License. You may obtain a copy of
 # the License at
-# 
+#
 # http://www.apache.org/licenses/LICENSE-2.0
-# 
+#
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
 # WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
@@ -14,7 +14,7 @@
 
 $accounts = gcloud iam service-accounts list --format=json | ConvertFrom-Json
 $appEngineAccount = $accounts | `
-    Where-Object displayName -eq 'App Engine default service account' 
+    Where-Object displayName -eq 'App Engine default service account'
 
 if (-not $appEngineAccount) {
     throw "Could not find the App Engine Default service account in $accounts"
@@ -22,7 +22,9 @@ if (-not $appEngineAccount) {
 
 $email = $appEngineAccount.email
 $projectId = $appEngineAccount.projectId
-$role = 'roles/cloudkms.admin'
-Write-Output "Adding role $role to $email."
-gcloud projects add-iam-policy-binding $projectId `
-    --member serviceAccount:$email --role $role
+$roles = @('roles/cloudkms.admin', 'roles/cloudkms.cryptoKeyEncrypterDecrypter')
+foreach ($role in $roles) {
+    Write-Output "Adding role $role to $email."
+    gcloud projects add-iam-policy-binding $projectId `
+        --member serviceAccount:$email --role $role
+}
